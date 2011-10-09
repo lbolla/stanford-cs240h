aca79a1011ef97f605ba1af9b91392feb8409075
Lorenzo Bolla <lbolla@gmail.com>
2011-10-09 16:29:55 +0100

    better comments

--- lab1/lab1.hs
+++ lab1/lab1.hs
@@ -1,6 +1,6 @@
 -- TODO
--- files from command line
--- strip in clearString
+-- read files from command line
+-- use "strip" in clearString instead of "elem"
 
 module Main where
 
@@ -8,29 +8,37 @@ import Data.List (sort, sortBy, group)
 import Data.Char (toLower)
 import Data.Map (assocs, fromListWith, foldrWithKey)
 
+-- Stores a word and its frequency in the text.
 data Token = Token { word :: String, count :: Int } deriving Show
 
+-- Max linewidth of output histogram.
 lineWidth = 80
+
+-- Characters stripped off each word.
 invalidChars = ",:;.!?"
 
+-- Strips invalid characters off a word.
 clearString :: String -> String
 clearString s = map toLower $ filter (`notElem` invalidChars) s
 
--- Using sort
+-- Given a (long) string, count the frequency of each word (using sort).
 countTokensSort :: String -> [Token]
 countTokensSort s = map 
 	(\x -> Token (head x) (length x)) 
 	(group . sort $ map clearString $ words s)
 
--- Using Data.Map
+-- Given a (long) string, count the frequency of each word (using Data.Map).
 countTokensMap :: String -> [Token]
 countTokensMap s = foldrWithKey
 	(\w n acc -> (Token w n):acc)
 	[]
 	(fromListWith (+) (zip (map clearString (words s)) (repeat 1)))
 
+-- Choose which implementation to use.
 countTokens = countTokensMap
 
+-- Print a Token as a word and a list of '#' representing its frequency.
+-- The length of the line will not exceed "lineWidth".
 printToken :: Int -> Int -> Token -> IO ()
 printToken maxLength maxCount (Token w c) = 
 	let lenWord = length w;
@@ -43,6 +51,7 @@ printToken maxLength maxCount (Token w c) =
 		w ++ 
 		(concat $ (replicate numSpaces " ") ++ (replicate numHashes "#"))
 
+-- Print the histogram of a list of Tokens
 printTokens :: [Token] -> IO ()
 printTokens tokens =
 	let maxLength = maximum $ map (length . word) tokens
