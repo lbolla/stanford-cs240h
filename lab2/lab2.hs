5598da759c77ee00d3e27c40c81969177f174baa
Lorenzo Bolla <lorenzo@zugoservices.com>
2011-10-13 16:43:48 +0100

    rudimental data structure for RTrees

--- lab2/lab2.hs
+++ lab2/lab2.hs
@@ -22,12 +22,24 @@ instance Ord Rectangle where
 	compare r1 r2 = compare (distance r1) (distance r2)
 
 -- Minimum Bounding Rectangle
-mbr :: [Rectangle] -> Rectangle
-mbr rects = Rectangle (Point xMin yMin) (Point xMax yMax)
+minimumBoundingRect :: [Rectangle] -> Rectangle
+minimumBoundingRect rects = Rectangle (Point xMin yMin) (Point xMax yMax)
 	where xMin = minimum $ map (xCoord . p1) rects
 	      xMax = maximum $ map (xCoord . p2) rects
 	      yMin = minimum $ map (yCoord . p1) rects
 	      yMax = maximum $ map (yCoord . p2) rects
+
+data LeafItem = LeafItem { rect :: Rectangle, hv :: Int } deriving Show
+data RTree = EmptyRTree
+	     | Node { mbr :: Rectangle, mhv :: Int, nodeCapacity :: Int, children :: [RTree] }
+	     | Leaf { leafCapacity :: Int, items :: [LeafItem] }
+	     deriving Show
+
+mkLeafItem :: Rectangle -> LeafItem
+mkLeafItem r = LeafItem r (distance r)
+
+mkLeaf :: Int -> [Rectangle] -> RTree
+mkLeaf c rects = Leaf c $ map mkLeafItem rects
 
 main = do
 	let r1 = Rectangle (Point 0 0) (Point 10 10)
@@ -36,4 +48,8 @@ main = do
 	putStrLn $ show $ distance r1
 	putStrLn $ show $ r1 == r2
 	putStrLn $ show $ r1 > r2
-	putStrLn $ show $ mbr [r1, r2]
+	putStrLn $ show $ minimumBoundingRect [r1, r2]
+	let l1 = mkLeafItem r1
+	putStrLn $ show l1
+	let t1 = mkLeaf 10 [r1, r2]
+	putStrLn $ show t1
