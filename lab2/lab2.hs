ed05e11fef26450b51a4d3c4cff9df472fc65001
Lorenzo Bolla <lbolla@gmail.com>
2011-10-16 22:14:41 +0100

    some more thoughts

--- lab2/lab2.hs
+++ lab2/lab2.hs
@@ -1,124 +1,123 @@
 module Main where
 
 import Data.Bits
-import Data.List (partition)
+import Data.List (partition, sort)
 import Hilbert
 
 data Point = Point { xCoord :: Int, yCoord :: Int } deriving (Eq, Show)
 data Rectangle = Rectangle {
-	p1 :: Point,
-	p2 :: Point
-} deriving (Eq, Show)
+        p1 :: Point,
+        p2 :: Point,
+        hv :: Int
+        } deriving (Eq, Show)
 
-center :: Rectangle -> Point
-center (Rectangle p1 p2) = Point xm ym
-	where xm = div (xCoord p1 + xCoord p2) 2
-	      ym = div (yCoord p1 + yCoord p2) 2
+hilbertOrder :: Int
+hilbertOrder = 16
 
-distance :: Rectangle -> Int
-distance r = hilbertDistance 16 $ (xCoord c, yCoord c)
-	where c = center r
+mkRectangle :: Point -> Point -> Rectangle
+mkRectangle p1 p2 = Rectangle p1 p2 hv
+        where hv = hilbertDistance hilbertOrder $ (xCoord c, yCoord c)
+              c = Point xm ym
+              xm = div (xCoord p1 + xCoord p2) 2
+              ym = div (yCoord p1 + yCoord p2) 2
 
 instance Ord Rectangle where
-	compare r1 r2 = compare (distance r1) (distance r2)
+        compare r1 r2 = compare (hv r1) (hv r2)
 
--- Minimum Bounding Rectangle
 minimumBoundingRect :: [Rectangle] -> Rectangle
-minimumBoundingRect rects = Rectangle (Point xMin yMin) (Point xMax yMax)
-	where xMin = minimum $ map (xCoord . p1) rects
-	      xMax = maximum $ map (xCoord . p2) rects
-	      yMin = minimum $ map (yCoord . p1) rects
-	      yMax = maximum $ map (yCoord . p2) rects
+minimumBoundingRect rects = mkRectangle (Point xMin yMin) (Point xMax yMax)
+        where xMin = minimum $ map (xCoord . p1) rects
+              xMax = maximum $ map (xCoord . p2) rects
+              yMin = minimum $ map (yCoord . p1) rects
+              yMax = maximum $ map (yCoord . p2) rects
 
-data LeafItem = LeafItem { rect :: Rectangle, hv :: Int } deriving Show
-data RTree = EmptyRTree
-	     | Node { mbr :: Rectangle, mhv :: Int, nodeCapacity :: Int, children :: [RTree] }
-	     | Leaf { leafCapacity :: Int, items :: [LeafItem] }
-	     deriving Show
 
-isLeafFull :: RTree -> Bool
-isLeafFull (Leaf c is) = c <= length is
-isLeafFull _ = error "called isLeafFull on non-leaf"
 
-mkLeafItem :: Rectangle -> LeafItem
-mkLeafItem r = LeafItem r $ distance r
+leafCapacity :: Int
+leafCapacity = 3
 
-singleton :: RTree
-singleton = EmptyRTree
+nonLeafCapacity :: Int
+nonLeafCapacity = 4
 
-insertRectInLeaf :: Rectangle -> RTree -> RTree
-insertRectInLeaf r (Leaf c is) = Leaf c $ iinf ++ [newLeaf] ++ isup
-	where newLeaf = mkLeafItem r
-	      (iinf, isup) = partition (\x -> (hv x) < (hv newLeaf)) is
-
-insert :: Rectangle -> RTree -> RTree
-insert r EmptyRTree = Leaf 10 $ [mkLeafItem r] -- TODO leaf capacity param of RTree
-insert r l@(Leaf c is)
-	| not (isLeafFull l) = insertRectInLeaf r l
-	| otherwise = error "TODO overflow"
-insert r (Node br h c cs) = error "TODO insert node"
-
-chooseLeaf :: RTree -> Rectangle -> RTree
-chooseLeaf l@(Leaf _ _) _ = l
-chooseLeaf t r = error "TODO"
-
-search :: RTree -> Rectangle -> [Rectangle]
-search EmptyRTree _ = []
-search (Leaf c is) r = filter (intersect r) (map rect is)
-search (Node br _ _ cs) r
-	| intersect r br = foldr (++) [] $ map (\t -> search t r) cs
-	| otherwise = []
+data NodeContent = NonLeafC Rectangle Int Forest | LeafC [Rectangle] deriving Show
+data RTree = Tip | Node NodeContent deriving Show
+type Forest = [RTree]
 
 intersect :: Rectangle -> Rectangle -> Bool
-intersect r1 r2 = not ( 
-		   r2_left > r1_right
-		|| r2_right < r1_left
-		|| r2_top < r1_bottom
-		|| r2_bottom > r1_top
-		)
-	where r2_left   = xCoord $ p1 r2
-	      r1_right  = xCoord $ p2 r1
-	      r2_right  = xCoord $ p2 r2
-	      r1_left   = xCoord $ p1 r1
-	      r2_top    = yCoord $ p2 r2
-	      r1_bottom = yCoord $ p1 r1
-	      r2_bottom = yCoord $ p1 r2
-	      r1_top    = yCoord $ p2 r1
+intersect r1 r2 = not (
+           r2_left > r1_right
+        || r2_right < r1_left
+        || r2_top < r1_bottom
+        || r2_bottom > r1_top
+        ) where r2_left   = xCoord $ p1 r2
+                r1_right  = xCoord $ p2 r1
+                r2_right  = xCoord $ p2 r2
+                r1_left   = xCoord $ p1 r1
+                r2_top    = yCoord $ p2 r2
+                r1_bottom = yCoord $ p1 r1
+                r2_bottom = yCoord $ p1 r2
+                r1_top    = yCoord $ p2 r1
+
+
+insert :: Rectangle -> RTree -> RTree
+insert r Tip = Node content
+        where content = LeafC [r]
+insert r (Node content) = case content of
+                               LeafC rects -> Node $ LeafC $ sort (r:rects)
+                               NonLeafC r mhv forest -> error "non-leaf"
+
+
+
+
+
+--  data LeafItem = LeafItem { rect :: Rectangle, hv :: Int } deriving Show
+--  data RTree = EmptyRTree
+--               | Node { mbr :: Rectangle, mhv :: Int, nodeCapacity :: Int, children :: [RTree] }
+--               | Leaf { leafCapacity :: Int, items :: [LeafItem] }
+--               deriving Show
+
+--  isLeafFull :: RTree -> Bool
+--  isLeafFull (Leaf c is) = c <= length is
+--  isLeafFull _ = error "called isLeafFull on non-leaf"
+
+--  mkLeafItem :: Rectangle -> LeafItem
+--  mkLeafItem r = LeafItem r $ distance r
+
+--  singleton :: RTree
+--  singleton = EmptyRTree
+
+--  insertRectInLeaf :: Rectangle -> RTree -> RTree
+--  insertRectInLeaf r (Leaf c is) = Leaf c $ iinf ++ [newLeaf] ++ isup
+--          where newLeaf = mkLeafItem r
+--                (iinf, isup) = partition (\x -> (hv x) < (hv newLeaf)) is
+
+--  insert :: Rectangle -> RTree -> RTree
+--  insert r EmptyRTree = Leaf 10 $ [mkLeafItem r] -- TODO leaf capacity param of RTree
+--  insert r l@(Leaf c is)
+--          | not (isLeafFull l) = insertRectInLeaf r l
+--          | otherwise = error "TODO overflow"
+--  insert r (Node br h c cs) = error "TODO insert node"
+
+--  chooseLeaf :: RTree -> Rectangle -> RTree
+--  chooseLeaf l@(Leaf _ _) _ = l
+--  chooseLeaf t r = error "TODO"
+
+--  search :: RTree -> Rectangle -> [Rectangle]
+--  search EmptyRTree _ = []
+--  search (Leaf c is) r = filter (intersect r) (map rect is)
+--  search (Node br _ _ cs) r
+--          | intersect r br = foldr (++) [] $ map (\t -> search t r) cs
+--          | otherwise = []
+
 
 main = do
-	let r0 = Rectangle (Point 100 100) (Point 110 110)
-	let r1 = Rectangle (Point 0 0) (Point 10 10)
-	let r2 = Rectangle (Point 0 0) (Point 8 12)
-	let r3 = Rectangle (Point 0 0) (Point 1 2)
-	--  putStrLn $ show $ center r1
-	--  putStrLn $ show $ distance r1
-	--  putStrLn $ show $ r1 == r2
-	--  putStrLn $ show $ r1 > r2
-	--  putStrLn $ show $ minimumBoundingRect [r1, r2]
-	--  let l1 = mkLeafItem r1
-	--  putStrLn $ show l1
-	--  let t1 = mkLeaf 10 [r1, r2]
-	--  putStrLn $ show t1
-	--  let t1 = insert r1 singleton
-	--  putStrLn $ show $ t1
-	--  let t2 = insert r2 t1
-	--  putStrLn $ show $ t2
-	--  let t3 = insert r3 t2
-	--  putStrLn $ show $ t3
-	--  let t0 = Node r1 (distance r1) 10 [Leaf 10 [mkLeafItem r1]]
+        let r0 = mkRectangle (Point 100 100) (Point 110 110)
+        let r1 = mkRectangle (Point 0 0) (Point 10 10)
+        let r2 = mkRectangle (Point 0 0) (Point 8 12)
+        let r3 = mkRectangle (Point 0 0) (Point 1 2)
+        --  putStrLn $ show r1
+        --  putStrLn $ show $ sort [r0, r1, r2, r3]
+        let l1 = Node $ LeafC [r0, r1, r2]
+        putStrLn $ show l1
 
-	let l1 = Leaf 10 [
-			mkLeafItem r1,
-			mkLeafItem r2
-		]
-	let l2 = Leaf 10 [
-			mkLeafItem r1,
-			mkLeafItem r3
-		]
-	let t0 = Node r1 (distance r1) 10 [l1, l2]
-	putStrLn $ show $ t0
-	
-	putStrLn $ show $ search EmptyRTree r1
-	putStrLn $ show $ search l1 r1
-	putStrLn $ show $ search t0 r1
-	putStrLn $ show $ search t0 r0
+
